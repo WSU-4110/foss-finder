10276764efe2190d5322bb3062a94e7f64a92487
Sahith Konuganti <hc9144@wayne.edu>
2023-03-23 19:53:36 -0400

    Pushing some changes for dark mode

--- AppPage/templates/script.js
+++ AppPage/templates/script.js
@@ -7,9 +7,9 @@ bottom: '15px', // default: '32px'
   backgroundColor: '#fff',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: false, // default: true,
   label: '🌓',  // default: ''
   autoMatchOsTheme: true // default: true
+
 }
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
