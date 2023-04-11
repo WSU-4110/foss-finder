475634193f10a09be0aa9e14978745252201b7bb
SahithKonuganti1 <123396453+SahithKonuganti1@users.noreply.github.com>
2023-04-11 01:02:10 -0400

    Unit tests for each webpage
    This checks whether or not the webpage's work.

--- FossFinder/AppPage/tests.py
+++ FossFinder/AppPage/tests.py
@@ -2,15 +2,15 @@ from django.test import TestCase
 from django.urls import reverse
 
 class NavigationBarTest(TestCase):
-    def test_navigation_bar(self):
+    def test_homepage(self):
         response = self.client.get(reverse('software_index'))
         self.assertEqual(response.status_code, 200)
-
+    def test_aboutpage(self)
         response = self.client.get(reverse('about'))
         self.assertEqual(response.status_code, 200)
-
+    def test_softwaresubmission(self)
         response = self.client.get(reverse('software_submit'))
         self.assertEqual(response.status_code, 200)
-
+    def test_reportbugs(self)
         response = self.client.get(reverse('bug_report'))
         self.assertEqual(response.status_code, 200)
