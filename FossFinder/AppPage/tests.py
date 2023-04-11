25c62bfc8a779f815c4accf117eab8f2069dde90
SahithKonuganti1 <123396453+SahithKonuganti1@users.noreply.github.com>
2023-04-11 00:59:34 -0400

    Created unit tests for the navigation bar.
    This was to check if each webpage was working properly.

--- FossFinder/AppPage/tests.py
+++ FossFinder/AppPage/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class NavigationBarTest(TestCase):
+    def test_navigation_bar(self):
+        response = self.client.get(reverse('software_index'))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(reverse('software_submit'))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(reverse('bug_report'))
+        self.assertEqual(response.status_code, 200)
