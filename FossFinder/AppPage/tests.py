12a4716bedc4161c90601ebe47226822453793fd
Carpetman2018 <laszloe16@gmail.com>
2023-04-13 23:13:43 -0400

    Added colons in Navigation Bar test class

--- FossFinder/AppPage/tests.py
+++ FossFinder/AppPage/tests.py
@@ -1,17 +1,18 @@
 from django.test import TestCase
 from django.urls import reverse
+from .views import ImageUrlParser
 
 class NavigationBarTest(TestCase):
     def test_homepage(self):
         response = self.client.get(reverse('software_index'))
         self.assertEqual(response.status_code, 200)
-    def test_aboutpage(self)
+    def test_aboutpage(self):
         response = self.client.get(reverse('about'))
         self.assertEqual(response.status_code, 200)
-    def test_softwaresubmission(self)
+    def test_softwaresubmission(self):
         response = self.client.get(reverse('software_submit'))
         self.assertEqual(response.status_code, 200)
-    def test_reportbugs(self)
+    def test_reportbugs(self):
         response = self.client.get(reverse('bug_report'))
         self.assertEqual(response.status_code, 200)
 
