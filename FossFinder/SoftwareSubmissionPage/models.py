0b0faea9792a576dabea21c80d671b30823659bf
Jack@Desktop <jacknishanian@gmail.com>
2023-04-20 10:59:50 -0400

    changed software cards + minor code cleaning

--- FossFinder/SoftwareSubmissionPage/models.py
+++ FossFinder/SoftwareSubmissionPage/models.py
@@ -6,6 +6,7 @@ class SubmittedSoftware(models.Model):
     developer = models.CharField(max_length=25)
     description = models.TextField()
 
+
     tag_WordProcesser = models.BooleanField(default=False)
     tag_SpreadSheet = models.BooleanField(default=False)
     tag_GraphicsEditor = models.BooleanField(default=False)
@@ -19,13 +20,9 @@ class SubmittedSoftware(models.Model):
     tag_Other = models.BooleanField(default=False)
 
 
-
     os_WIN = models.BooleanField(default=False)
     os_MAC = models.BooleanField(default=False)
     os_NIX = models.BooleanField(default=False)
 
-    #image = models.ImageField(upload_to="img/")
+
     image = models.URLField(max_length=200)
-    # url feild worked to upload to database
-    # while image feild works to view image
-    # id 1 has imagefield 2 has urlfield
